b663e602630a0421ebf6d79042b898c47d6d92b8
koertzen <adrian.koertzen@o2.com>
2014-03-04 16:07:26 +0000

    first commit, create app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bark::Application.config.secret_key_base = '9ba2b238fed5f9ffbbaadbc05547ea8e535f569795eb84a719105b7ab9589f57dad10ad2bf55dcef2d1387479d46c491823276ac5818428a40124705f7152bcb'
