d5401694e45493af57545c28ac960ce09bd92ec8
koertzen <adrian.koertzen@o2.com>
2014-03-18 10:46:33 +0000

    Fixed the bad copy paste code - working

--- app/controllers/woofs_controller.rb
+++ app/controllers/woofs_controller.rb
@@ -1,22 +1,22 @@
-class	WoofsController	<	ApplicaHonController	
-		def	create	
-				@woof	=	Woof.new(woof_params)	
-				@woof.user_id	=	current_user.id	
-				
-				if	@woof.save	
-								redirect_to	current_user		
-				else	
-								ﬂash[:error]	=	"Problem!"	
-								redirect_to	current_user	
-				end	
-		end	
+class	WoofsController	<	ApplicationController	
+def	create	
+	@woof	=	Woof.new(woof_params)	
+	@woof.user_id	=	current_user.id	
 	
-		private	
-				def	set_woof	
-						@woof	=	Woof.ﬁnd(params[:id])	
-				end	
-	
-				def	woof_params	
-						params.require(:woof).permit(:content,	:user_id)	
-				end	
+	if @woof.save
+		redirect_to current_user
+	else
+		flash[:error] = "Problem!"
+		redirect_to current_user
+	end
+end	
+
+private	
+	def	set_woof	
+			@woof = Woof.find(params[:id])
+	end	
+
+	def	woof_params	
+			params.require(:woof).permit(:content,	:user_id)	
+	end	
 end	
